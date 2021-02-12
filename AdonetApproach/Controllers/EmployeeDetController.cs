8365ec2f559ba5154ad0c678c65ecb9e99396199
yawar2019 <yawarali17@gmail.com>
2021-02-12 08:10:43 +0530

    Commit Code

---
+++ AdonetApproach/Controllers/EmployeeDetController.cs
@@ -0,0 +1,20 @@
+﻿using AdonetApproach.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdonetApproach.Controllers
+{
+    public class EmployeeDetController : Controller
+    {
+        EmployeeContext db = new EmployeeContext();
+
+        // GET: EmployeeDet
+        public ActionResult Index()
+        {
+            return View(db.getEmployee());
+        }
+    }
+}
---
+++ AdonetApproach/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AdonetApproach.MvcApplication" Language="C#" %>
