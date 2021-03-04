7e8c8ea5653427900cc1adf7480bbe4a7ecb40b9
yawar2019 <yawarali17@gmail.com>
2021-03-04 08:08:28 +0530

    committed

---
+++ MVC7amThundersBatch/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using MVC7amThundersBatch.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MVC7amThundersBatch.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        EmployeeEntities db = new EmployeeEntities();
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(UserDetail reg)
+        {
+            UserDetail user = db.UserDetails.Where(s => s.UserName == reg.UserName && s.Password == reg.Password).SingleOrDefault();
+
+            if (user != null)
+            {
+                FormsAuthentication.SetAuthCookie(reg.UserName, false);
+                return Redirect("~/Login/Index");
+            }
+            else
+            {
+                return View();
+
+            }
+        }
+
+        public ActionResult SignOut()
+        {
+            FormsAuthentication.SignOut();
+            return Redirect("~/Login/Login");
+        }
+    }
+}
