70d12ab20a51e0faff42a976a14115f374bde1c0
yawar2019 <yawarali17@gmail.com>
2021-03-26 07:38:42 +0530

    commit

---
+++ LaundrySysstem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LaundrySysstem.MvcApplication" Language="C#" %>
---
+++ MVC7amThundersBatch/Areas/Manager/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC7amThundersBatch.Areas.Manager.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Manager/Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
